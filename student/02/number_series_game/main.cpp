1866e185661cca0f33191fab3550962dbf4e1927
Juliusz Kotelba <juliusz.kotelba@tuni.fi>
2022-01-25 09:40:20 +0200

    zipboing

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-
+using namespace std;
 
 int main()
 {
-    std::cout << "How many numbers would you like to have? ";
-
+    int loppu = 0;
+    cout << "How many numbers would you like to have? ";
+    cin >> loppu;
+    for (int luku = 0; luku < loppu; luku++) {
+        if (((luku + 1) % 3) == 0 and ((luku+1)%7)==0) {
+            cout << ("zip boing") << endl;
+        } else if (((luku+1) % 7) == 0) {
+            cout << ("boing") << endl;
+        } else if (((luku + 1) % 3) == 0) {
+            cout << ("zip") << endl;
+        } else {
+            cout << (luku + 1)<< endl;
+        }
+    }
     return 0;
 }
