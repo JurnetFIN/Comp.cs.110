da0ec26239848224aa13124eb6779329aeb810f3
Juliusz Kotelba <juliusz.kotelba@tuni.fi>
2022-01-30 22:30:22 +0200

    Palautus 2.3.2

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    cout << "Enter a positive number: ";
+    int luku = 0;
+    int a = 0;
+    int b = 0;
+    
+    while("True") {
+        cout << "Enter a positive number: ";
+        cin >> luku;
+        if (luku <= 0) {
+            cout << "Only positive numbers accepted" << endl;
+            break;
+        }
+        else {
+            break;
+        }
+    }
 
+    a = sqrt(luku);
+
+    while("True") {
+        b = luku / a;
+        if (b*a==luku) {
+            cout << luku << " = " << a << " * " << b << endl;
+            break;
+        }
+        else {
+            a -= 1;
+        }
+    }
+    
     return 0;
 }
