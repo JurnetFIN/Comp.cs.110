f23f2128621ea2f8d4a0ca0af17c315dab9bfe2d
Juliusz Kotelba <juliusz.kotelba@tuni.fi>
2022-02-11 13:18:53 +0200

    Palautus 4.2.2

--- student/04/split/main.cpp
+++ student/04/split/main.cpp
@@ -2,10 +2,28 @@
 #include <string>
 #include <vector>
 
+std::vector< std::string > split(std::string line, char seperator, bool a=false)
+{
+    std::vector< std::string > new_line;
 
-// TODO: Implement split function here
-// Do not change main function
+    int b = 0;
 
+    for(char i : line) {
+        if (i != seperator) {
+            std::string tmp_string;
+            tmp_string.insert(0, 1, i);
+
+            new_line.push_back(tmp_string);
+
+        } else {
+            if (a == false) {
+                new_line.push_back("");
+            }
+        b += 1;
+        }
+    }
+    return new_line;
+}
 
 int main()
 {
@@ -15,13 +33,14 @@ int main()
     std::cout << "Enter the separator character: ";
     char separator = getchar();
 
-    std::vector< std::string > parts  = split(line, separator);
+
+    std::vector<std::string> parts = split(line, separator);
     std::cout << "Splitted string including empty parts: " << std::endl;
     for( auto part : parts ) {
         std::cout << part << std::endl;
     }
 
-    std::vector< std::string > parts_no_empty  = split(line, separator, true);
+    std::vector<std::string> parts_no_empty = split(line, separator, true);
     std::cout << "Splitted string ignoring empty parts: " << std::endl;
     for( auto part : parts_no_empty ) {
         std::cout << part << std::endl;
