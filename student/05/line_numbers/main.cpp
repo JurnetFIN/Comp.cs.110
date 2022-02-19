63530ec1f82323006fb486ea5b43113a0e120bbb
Juliusz Kotelba <juliusz.kotelba@tuni.fi>
2022-02-19 21:34:46 +0200

    5.1.1. palautus

--- student/05/line_numbers/main.cpp
+++ student/05/line_numbers/main.cpp
@@ -10,16 +10,16 @@ int main() {
     cout << "Input file: ";
     getline(cin, tiedoston_nimi);
 
-    ifstream tiedosto(tiedoston_nimi);
-    if ( not tiedosto ) {
+    ifstream tiedosto_olio(tiedoston_nimi);
+    if ( not tiedosto_olio ) {
         cout << "Error! The file " << tiedoston_nimi << " cannot be opened." << endl;
     } else {
         vector<string> rivit_tiedostossa;
         string rivi;
-        while (getline(tiedosto, rivi)) {
+        while (getline(tiedosto_olio, rivi)) {
             rivit_tiedostossa.push_back(rivi);
         }
-        tiedosto.close();
-        cout << rivit_tiedostossa.at(1) << endl;
+        tiedosto_olio.close();
+        cout << rivit_tiedostossa.at(0) << endl;
     }
 }
