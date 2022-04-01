dbe12226578088e836907392b03d55c1992029e1
Juliusz Kotelba <juliusz.kotelba@tuni.fi>
2022-04-01 22:49:10 +0300

    Suunnistus - MAP

--- student/10/orienteering2/orienteeringmap.cpp
+++ student/10/orienteering2/orienteeringmap.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <map>
 #include <memory>
+#include <vector>
 
 using namespace std;
 
@@ -21,14 +22,13 @@ void OrienteeringMap::set_map_size(int width, int height) {
 // coordinates), height and marker.
 void OrienteeringMap::add_point(string name, int x, int y, int height, char marker) {
 
-    if ((x < 0) or (x > width_)) {
+    if ((x < 0) or (x > width_))
         return;
-    }
 
-    if ((y < 0) or (y > height_)) {
+    if ((y < 0) or (y > height_))
         return;
-    }
 
+    // Lisataan rasti mapille, jos se ei ole olemassa
     if (points_.find(marker) == points_.end()) {
         shared_ptr<Point> new_point(new Point{name, x, y, height, marker});
 
@@ -47,21 +47,24 @@ bool OrienteeringMap::connect_route(string from,
                    string to,
                    string route_name) {
 
+    // Haetaan muuttujat
     shared_ptr<Point> from_ptr = nullptr;
     shared_ptr<Point> to_ptr = nullptr;
 
+    // Tarkistetaan onko molemmat rastit olemassa
     for(auto& p: points_) {
-        if (p.second->name == from) {
+        if (p.second->name == from)
             from_ptr = p.second;
-        }
-        if (p.second->name == to) {
+
+        if (p.second->name == to)
             to_ptr = p.second;
-        }
-    }
-    if ((from_ptr == nullptr) or (to_ptr == nullptr) ){
-        return false;
     }
 
+    // Jos jompikumpi ei ole olemassa -> Virhe
+    if ((from_ptr == nullptr) or (to_ptr == nullptr))
+        return false;
+
+    // Lisataan reitti
     if (routes_.find(route_name) == routes_.end()) {
         shared_ptr<Route> new_route(new Route());
         routes_.insert(pair<string, shared_ptr<Route>>(route_name, new_route));
@@ -74,7 +77,42 @@ bool OrienteeringMap::connect_route(string from,
 
 // Prints the map.
 void OrienteeringMap::print_map() const {
+    // Muodostetaan "kartta", jossa on vain pisteita
+    vector<vector<char>> points;
+    for(int x=1; x <=width_; x++) {
+        vector<char> chars;
+        for(int y=1; y <=height_; y++) {
+            chars.push_back('.');
+        }
+        points.push_back(chars);
+    }
 
+    // Korvataan pisteet rasteilla
+    for(auto& p: points_)
+        points.at((p.second->x)-1).at((p.second->y)-1) = p.second->marker;
+
+    // Tulostetaan kartta
+    // Ensin tulostetaan ylapalkki
+    cout << " ";
+    string prefix = "  ";
+    for(int x=1; x <=width_; x++) {
+        if(x>=10)
+            prefix = " ";
+        cout << prefix << x;
+    }
+    cout << endl;
+
+    // Sen jalkeen tulostetaan rivi kerrallaan
+    for(int y=1; y <= height_; y++) {
+        if(y>=10)
+            prefix = "";
+        cout << prefix << y;
+
+        for(int x=1; x <= width_; x++) {
+            cout << "  " << points[x-1][y-1];
+        }
+        cout << endl;
+    }
 };
 
 // Prints all routes' names, ordered alphabetically.
