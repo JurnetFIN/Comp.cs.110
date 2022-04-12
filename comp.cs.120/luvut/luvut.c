9d842c5328568a2a3f047e068b560e0dce0868cd
Juliusz Kotelba <juliusz.kotelba@tuni.fi>
2022-04-12 16:35:36 +0300

    Luvut

--- comp.cs.120/luvut/luvut.c
+++ comp.cs.120/luvut/luvut.c
@@ -6,13 +6,10 @@ int main()
     int i = 1;
     for(i=1; i <= num; ++i) {
         if (i % 2 == 0) {
-            //printf("%d", i);
             continue;
         } else if (i % 3 == 0) {
-            //printf("%d", i);
             continue;
         } else if (i % 5 == 0) {
-            //printf("%d", i);
             continue;
         } else {
             printf("%d", i);
