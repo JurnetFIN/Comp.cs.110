763f2b46849dd37ce9b40a65c5e05c429fd12e78
Juliusz Kotelba <juliusz.kotelba@tuni.fi>
2022-04-13 19:15:18 +0300

    Ascii

--- comp.cs.120/ascii/ascii.c
+++ comp.cs.120/ascii/ascii.c
@@ -4,19 +4,17 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc == 2) {
-        int start = atoi(argv[0]);
-        int end = atoi(argv[1]);
-        int i;
-        scanf("%d", &start);
-        scanf("%d", &end);
-        for(i=start; i<=end; ++i) {
-            printf("%d", i);
-            printf(": ");
-            if((i > 32) && (i < 126))
-                printf("%c", i);
-            printf("\n");
-        }
+    int start = atoi(argv[0]);
+    int end = atoi(argv[1]);
+    int i;
+    scanf("%d", &start);
+    scanf("%d", &end);
+    for(i=start; i<=end; ++i) {
+        printf("%d", i);
+        printf(": ");
+        if((i > 32) && (i < 126))
+            printf("%c", i);
+        printf("\n");
     }
     return 0;
 }
