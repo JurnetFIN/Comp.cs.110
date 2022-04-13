c17f18e63b47872d2fed794a6832e1068ebef402
Juliusz Kotelba <juliusz.kotelba@tuni.fi>
2022-04-13 18:46:25 +0300

    ASCII

--- comp.cs.120/komentorivi/ascii.c
+++ comp.cs.120/komentorivi/ascii.c
@@ -2,14 +2,12 @@
 
 int main()
 {
-    char chr;
     int start;
     int end;
+    int i;
     scanf("%d", &start);
     scanf("%d", &end);
-
-    int i;
-    for(i=start; i<end; ++i) {
+    for(i=start; i<=end; ++i) {
         printf("%d", i);
         printf(": ");
         if((i > 32) && (i < 126))
